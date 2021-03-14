093d6a9ecff796c4c0818fdf4ccb75e4bfe41d17
Igboanugwo Collins <munisco12@gmail.com>
2021-03-14 22:57:12 +0100

    working with interfaces

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,11 @@
-console.log('hello world!');
+const Vehicle = {
+    name: 'camry',
+    year: 2019,
+    summary() {
+        return `The year is ${this.year}`;
+    }
+};
+const printSummary = (item) => {
+    console.log(`The year of the car is  ${item.summary}`);
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello world!')
+
+const Vehicle = {
+    name: 'camry',
+    year: 2019,
+    summary(): string {
+        return `The year is ${this.year}`
+    }
+}
+
+
+interface Report {
+    summary(): string
+}
+
+
+const printSummary = (item: Report) => {
+    console.log(`The year of the car is  ${item.summary}`)
+}
