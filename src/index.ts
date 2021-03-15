cc93c92782d2a906baebd5813241d1ded131714f
Igboanugwo Collins <munisco12@gmail.com>
2021-03-15 08:54:50 +0100

    working on classes

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,41 @@
 
-const Vehicle = {
-    name: 'camry',
-    year: 2019,
-    summary(): string {
-        return `The year is ${this.year}`
+// const Vehicle = {
+//     name: 'camry',
+//     year: 2019,
+//     summary(): string {
+//         return `The year is ${this.year}`
+//     }
+// }
+
+
+// interface Report {
+//     summary(): string,
+//     name: string
+// }
+
+
+// const printSummary = (item: Report) => {
+//     console.log(`The summary is ${item.summary()}`)
+//     console.log('the name of the car is ', item.name)
+// }
+
+// printSummary(Vehicle)
+
+
+class Vehicle {
+    drive(){
+        console.log('driving!!')
+    }
+
+    move(){
+        console.log('moving')
     }
 }
 
-
-interface Report {
-    summary(): string,
-    name: string
+class Car extends Vehicle{
+    drive(){
+        console.log('driving car')
+    }
 }
 
-
-const printSummary = (item: Report) => {
-    console.log(`The summary is ${item.summary()}`)
-    console.log('the name of the car is ', item.name)
-}
-
-printSummary(Vehicle)
+const car = new Car()
