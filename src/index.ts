712178d9178f6a5ee79471f2e00864b05e1850a2
Igboanugwo Collins <munisco12@gmail.com>
2021-03-15 17:15:51 +0100

    Starting map project

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,55 @@
 
-// const Vehicle = {
-//     name: 'camry',
-//     year: 2019,
-//     summary(): string {
-//         return `The year is ${this.year}`
+// // const Vehicle = {
+// //     name: 'camry',
+// //     year: 2019,
+// //     summary(): string {
+// //         return `The year is ${this.year}`
+// //     }
+// // }
+
+
+// // interface Report {
+// //     summary(): string,
+// //     name: string
+// // }
+
+
+// // const printSummary = (item: Report) => {
+// //     console.log(`The summary is ${item.summary()}`)
+// //     console.log('the name of the car is ', item.name)
+// // }
+
+// // printSummary(Vehicle)
+
+
+// class Vehicle {
+
+//     constructor(protected color: string){}
+//     protected drive(){
+//         console.log('driving Vehicle wih', this.color, 'color')
+//     }
+
+//     move(){
+//         console.log('moving')
 //     }
 // }
 
+// class Car extends Vehicle{
+//     // drive(){
+//     //     console.log('driving car')
+//     // }
+//     constructor(public wheels: number, color:string){
+//         super(color);
+//         {
+//             console.log('wheels', this.wheels)
+//         }
+//     }
 
-// interface Report {
-//     summary(): string,
-//     name: string
+//     showDrive(): void{
+//         this.drive()
+//     }
 // }
 
-
-// const printSummary = (item: Report) => {
-//     console.log(`The summary is ${item.summary()}`)
-//     console.log('the name of the car is ', item.name)
-// }
-
-// printSummary(Vehicle)
-
-
-class Vehicle {
-
-    constructor(protected color: string){}
-    protected drive(){
-        console.log('driving Vehicle wih', this.color, 'color')
-    }
-
-    move(){
-        console.log('moving')
-    }
-}
-
-class Car extends Vehicle{
-    // drive(){
-    //     console.log('driving car')
-    // }
-    constructor(public wheels: number, color:string){
-        super(color);
-        {
-            console.log('wheels', this.wheels)
-        }
-    }
-
-    showDrive(): void{
-        this.drive()
-    }
-}
-
-const car = new Car(4, 'red')
-car.showDrive()
-car.move()
+// const car = new Car(4, 'red')
+// car.showDrive()
+// car.move()
