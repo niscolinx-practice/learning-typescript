77b6867d3c87d2c3cd0f18cf97a37fbec764abbb
Igboanugwo Collins <munisco12@gmail.com>
2021-03-15 18:17:14 +0100

    working on user class

--- src/User.ts
+++ src/User.ts
@@ -1,7 +1,15 @@
+import faker from 'faker'
+
 class User {
     name: string;
     location: {
         lat: string;
         lng: string
+    }
+
+    constructor(){
+        this.name = faker.name.firstName()
+        this.location.lat = faker.address.latitude()
+        this.location.lng = faker.address.longitude()
     }
 }
