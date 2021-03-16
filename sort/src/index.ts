0ef51489a44f4828d61f12159bfd5a766daf8c34
Igboanugwo Collins <munisco12@gmail.com>
2021-03-16 20:54:58 +0100

    implementing bubblesort

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello world')
+// Bubble sort 
+
+
+const arr = [5, 4, -2, 1, 0,6,3]
+
+
+const bubbleSort = (arr: number[]) => {
+
+    let toSort = arr.length
+    for(let i = 0; i < toSort; i++){
+
+        for(let j = i + 1; j < i + 2; j++){
+            
+        }
+    }
+}
