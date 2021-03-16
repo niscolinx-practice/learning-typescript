38326e89c759f73ecfde1611c8aad826e8baddf1
Igboanugwo Collins <munisco12@gmail.com>
2021-03-16 17:18:22 +0100

    working on customMap in order to restrict access

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -123159,7 +123159,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40845" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35647" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
