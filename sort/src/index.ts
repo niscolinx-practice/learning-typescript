4caeeba5b36909ddbc11c0c6027cfcb8810cf200
Igboanugwo Collins <munisco12@gmail.com>
2021-03-16 21:09:02 +0100

    implemented bubblesort

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -9,8 +9,25 @@ const bubbleSort = (arr: number[]) => {
     let toSort = arr.length
     for(let i = 0; i < toSort; i++){
 
-        for(let j = i + 1; j < i + 2; j++){
+        console.log(arr[i])
+
+        for(let j = i + 1; j < arr.length; j++){
+
+            console.log(arr[j])
             
+            if(arr[j] < arr[i]){
+
+                console.log('to sort', arr[j], arr[i])
+                let biggerNum = arr[i]
+                arr[i] = arr[j]
+                arr[j] = biggerNum
+            }
         }
     }
+
+    console.log(arr)
+    return arr
 }
+
+bubbleSort(arr)
+
