7876e558820c5375dcde27b3b2114d2e6b0c3f15
Igboanugwo Collins <munisco12@gmail.com>
2021-03-21 17:20:13 +0100

    Working on a web framework

--- webFramework/src/User.ts
+++ webFramework/src/User.ts
@@ -1,17 +1,30 @@
 interface UserProps {
     name: string
     age: number
+    level?: string
 }
 
 export class User {
-    constructor(private data: UserProps) {}
+    constructor(private data: UserProps) {
+        this.data = {
+            name: 'Clinton', 
+            age: 21,
+            level: '400L'
+        }
+    }
 
-    get(propName: string): number | string {
+    get(propName: string): number | string | undefined {
         return this.data[propName as keyof UserProps]
     }
 
-    set(userProp: UserProps): void{
-        this.data.age = userProp.age,
-        this.data.name = userProp.name
+    set(updateUserProp: UserProps): void{
+    //    this.data = {
+    //        ...this.data,
+    //        updateUserProp
+    //    }
+
+    Object.assign(this.data, updateUserProp)
+
+       console.log(this.data)
     }
 }
--- webFramework/src/index.ts
+++ webFramework/src/index.ts
@@ -3,4 +3,4 @@ import { User } from "./User";
 const user = new User({name: 'Collins', age: 52})
 
 user.set({name: 'Hello', age: 12})
-console.log(user.get('name'))
+console.log(user.get('age'))
