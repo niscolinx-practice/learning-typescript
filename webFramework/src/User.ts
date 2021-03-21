314fc7197b00d3a448b8fa4b078d5b84badfde30
Igboanugwo Collins <munisco12@gmail.com>
2021-03-21 17:02:43 +0100

    Working on a web framework

--- webFramework/src/User.ts
+++ webFramework/src/User.ts
@@ -9,4 +9,9 @@ export class User {
     get(propName: string): number | string {
         return this.data[propName]
     }
+
+    set(userProp: UserProps): UserProps{
+        this.data.age = userProp.age,
+        this.data.name = userProp.name
+    }
 }
