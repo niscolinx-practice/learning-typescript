66ef9f228d12b5202478193d09ee6665434a9487
Igboanugwo Collins <munisco12@gmail.com>
2021-03-22 18:08:14 +0100

    Separated all my server logic to Sync

--- webFramework/src/models/Sync.ts
+++ webFramework/src/models/Sync.ts
@@ -5,17 +5,17 @@ interface HasId {
 }
 export class Sync<T extends HasId> {
     constructor(private rootUrl: string, private data: T) {}
-    save(): void {
+    save(): Promise<AxiosResponse>{
         const { id } = this.data
 
         if (id) {
-            axios.put(this.rootUrl + id, this.data)
+           return  axios.put(this.rootUrl + id, this.data)
         } else {
-            axios.post(this.rootUrl, this.data)
+           return axios.post(this.rootUrl, this.data)
         }
     }
 
-    fetch(): Promise<AxiosResponse<any>>d {
+    fetch(): Promise<AxiosResponse> {
         return axios.get(this.rootUrl + this.data)
     }
 }
