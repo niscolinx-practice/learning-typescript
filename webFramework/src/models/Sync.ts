27db9b239c734c506a654be2bba2ddbadbcae696
Igboanugwo Collins <munisco12@gmail.com>
2021-03-22 17:56:39 +0100

    Added composition in my events

---
+++ webFramework/src/models/Sync.ts
@@ -0,0 +1,29 @@
+class Sync {
+    save(): void {
+        const id = this.get('id')
+
+        if (id) {
+            console.log(URL + id)
+            axios
+                .put(URL + id, this.data)
+                .then((res) => console.log(res))
+                .catch((err) => console.log(err))
+        } else {
+            axios
+                .post(URL, this.data)
+                .then((res) => console.log(res))
+                .catch((err) => console.log(err))
+        }
+    }
+
+    fetch(): void {
+        axios
+            .get(URL + this.get('id'))
+            .then((res: AxiosResponse) => {
+                this.set(res.data)
+            })
+            .catch((err) => {
+                throw new Error(err)
+            })
+    }
+}
