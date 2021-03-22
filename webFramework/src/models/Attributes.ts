eb699d35abcb5cffab727f0ed5218e9b1a5c8bd2
Igboanugwo Collins <munisco12@gmail.com>
2021-03-22 22:14:38 +0100

    Separated all my server logic to Sync

--- webFramework/src/models/Attributes.ts
+++ webFramework/src/models/Attributes.ts
@@ -2,8 +2,8 @@
 export class Attributes<T> {
     constructor(private data: T) {}
 
-    get<K extends keyof T>(propName: K): T[K] {
-        return this.data[propName]
+    get<K extends keyof T>(key: K): T[K] {
+        return this.data[key]
     }
 
     set(updateUserProp: T): void {
