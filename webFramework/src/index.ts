d72fc7086c80c1b7f88645e7f050e4e8bb67dc5f
Igboanugwo Collins <munisco12@gmail.com>
2021-03-22 14:32:53 +0100

    Working on trigger functions

--- webFramework/src/index.ts
+++ webFramework/src/index.ts
@@ -1,9 +1,9 @@
 import { userInfo } from 'node:os'
 import { User } from './models/User'
 
-//const user = new User({ name: 'Nelon', age: 23 })
+const user = new User({ name: 'Nelon', age: 23 })
 
-//console.log(user)
+console.log(user)
 //user.save()
 
 
