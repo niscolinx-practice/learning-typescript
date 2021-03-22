19b73ffc43876fd6c2f1a21508c8df8637d5d4fe
Igboanugwo Collins <munisco12@gmail.com>
2021-03-22 18:16:02 +0100

    Separated all my server logic to Sync

---
+++ webFramework/src/models/Attributes.ts
@@ -0,0 +1,11 @@
+export class Attributes {
+    constructor(private data: UserProps) {}
+
+    get(propName: string): number | string | undefined {
+        return this.data[propName as keyof UserProps]
+    }
+
+    set(updateUserProp: UserProps): void {
+        Object.assign(this.data, updateUserProp)
+    }
+}
