efba2840304faeb45cbda5ea33f8fd53104db9ef
Igboanugwo Collins <munisco12@gmail.com>
2021-03-23 22:15:38 +0100

    building user collection

--- webFramework/src/models/Collection.ts
+++ webFramework/src/models/Collection.ts
@@ -1,12 +1,12 @@
 import { User, UserProps } from './User'
 import axios from 'axios'
 
-export class Collection<User, UserProps> {
+export class Collection<User> {
     models: UserProps[] = []
 
     constructor(private rootUrl: string) {}
 
-    async fetch(): Promise<UserProps> {
+    async fetch(): Promise<User> {
         const allUsers = await axios.get(this.rootUrl)
         console.log(allUsers.data)
         return this.models = allUsers.data
