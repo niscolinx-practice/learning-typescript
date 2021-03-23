cdcbcaa8ce01ef5b237404605e0dc8da6e3699d2
Igboanugwo Collins <munisco12@gmail.com>
2021-03-23 22:06:12 +0100

    building user collectionn

--- webFramework/src/models/Collection.ts
+++ webFramework/src/models/Collection.ts
@@ -2,8 +2,15 @@ import { User, UserProps } from './User';
 import axios from 'axios'
 
 export class Collection<User, UserProps> {
+
+    models: UserProps[]
     
     constructor(private rootUrl: string ){
     
+    }
+
+    async fetch(){
+       const allUsers =  await axios.get(this.rootUrl)
+       
     }
 }
