3b4d956cc678f640fc932c8553b73342dd156701
Igboanugwo Collins <munisco12@gmail.com>
2021-03-23 16:22:58 +0100

    working on attrs

--- webFramework/src/models/UserModel.ts
+++ webFramework/src/models/UserModel.ts
@@ -1,17 +1,19 @@
-import { AxiosPromise } from 'axios';
+import { AxiosPromise } from 'axios'
 interface Sync<T> {
     save(data: T): AxiosPromise
 
     fetch(id: number): AxiosPromise
 }
-
 interface Attrs<T> {
     get<K extends keyof T>(key: K): T[K]
 
     set(update: T): void
 }
 
-export class UserModel {
+interface Events {
+    on(eventName: string, callback: () => void): void
 
-
+    trigger(eventName: string): void
 }
+
+export class UserModel<T> {}
