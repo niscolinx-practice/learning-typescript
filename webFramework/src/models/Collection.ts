db5b4ab6c4991eda37c80f4195b6510e0a0a5c64
Igboanugwo Collins <munisco12@gmail.com>
2021-03-24 16:53:28 +0100

    Working on generic collection

--- webFramework/src/models/Collection.ts
+++ webFramework/src/models/Collection.ts
@@ -1,6 +1,5 @@
 import { Events } from './Events'
 import axios, { AxiosResponse } from 'axios'
-import { User, UserProps } from './User'
 
 export class Collection<T, K> {
     models: T[] = []
@@ -8,7 +7,7 @@ export class Collection<T, K> {
 
     constructor(private rootUrl: string, private deserialize: (json: K) => T) {}
 
-    fetch(): Promise<void> {
+    fetch(): void {
         axios.get(this.rootUrl).then((res: AxiosResponse): void => {
             res.data.forEach((eachUser: K) => {
                 this.models.push(this.deserialize(eachUser))
