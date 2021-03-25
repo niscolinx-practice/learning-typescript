aede06732bc2490d41a78a9dca0c94cea6a382f1
Igboanugwo Collins <munisco12@gmail.com>
2021-03-25 15:33:50 +0100

    Working on user form in order to render html

--- webFramework/src/dist/src.77de5100.js
+++ webFramework/src/dist/src.77de5100.js
@@ -2291,7 +2291,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34637" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42575" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
