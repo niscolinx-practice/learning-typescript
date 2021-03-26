b315a505783c66ef506ed09ad1eca1218864c637
Igboanugwo Collins <munisco12@gmail.com>
2021-03-26 19:52:02 +0100

    Solving Louis Assignment

--- webFramework/src/models/ResponseDeserializer.ts
+++ webFramework/src/models/ResponseDeserializer.ts
@@ -1,4 +1,4 @@
-const jsonData = {
+const responseData = {
     notifications: [
         '{{172edb5e-0434-fb4b-abec-96bedc814599}} mentioned you in a post \n{{173d80bc-a995-c2ba-3d80-61d641b482df}}',
         '{{173d8098-ea25-dc4c-2c28-d1691ed8ea13}} sent you a friend request.',
@@ -18,3 +18,22 @@ const jsonData = {
         },
     },
 }
+
+interface Response {
+    notifications: string[],
+    entities : {
+        [key: string] : {
+            subject: string,
+            type: string,
+        }
+    }
+}
+
+const dataDeserializer = (response: Response): string => {
+
+
+    return ''
+}
+
+
+dataDeserializer(responseData)
