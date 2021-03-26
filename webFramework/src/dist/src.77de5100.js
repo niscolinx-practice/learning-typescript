56a35d0072c4daab3e072d2d456fd93138668d06
Igboanugwo Collins <munisco12@gmail.com>
2021-03-26 22:59:07 +0100

    done with louis assigment

--- webFramework/src/dist/src.77de5100.js
+++ webFramework/src/dist/src.77de5100.js
@@ -212,7 +212,7 @@ var _ResponseDeserializer = require("./models/ResponseDeserializer");
 // import { UserForm } from './models/UserForm'
 // const userForm = new UserForm(document.getElementById('root'))
 // userForm.render()
-console.log((0, _ResponseDeserializer.dataDeserializer)("{{172edb5e-0434-fb4b-abec-96bedc814599}} mentioned you in a post \n{{173d80bc-a995-c2ba-3d80-61d641b482df}}"));
+console.log((0, _ResponseDeserializer.dataDeserializer)('{{173d8098-ea25-dc4c-2c28-d1691ed8ea13}} sent you a friend request.'));
 },{"./models/ResponseDeserializer":"models/ResponseDeserializer.ts"}],"../../../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- webFramework/src/index.ts
+++ webFramework/src/index.ts
@@ -5,4 +5,8 @@ import { dataDeserializer } from './models/ResponseDeserializer';
 
 // userForm.render()
 
-console.log(dataDeserializer("{{172edb5e-0434-fb4b-abec-96bedc814599}} mentioned you in a post \n{{173d80bc-a995-c2ba-3d80-61d641b482df}}"))
+console.log(
+    dataDeserializer(
+        '{{173d8098-ea25-dc4c-2c28-d1691ed8ea13}} sent you a friend request.'
+    )
+)
