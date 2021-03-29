03008a3759456162b2f85432d2f7915099f08a2c
Igboanugwo Collins <munisco12@gmail.com>
2021-03-29 10:27:36 +0100

    working on user form

---
+++ webFramework/src/views/UserEdit.ts
@@ -0,0 +1,41 @@
+
+
+export class UserEdit {
+    constructor(public parent: HTMLElement | null, public model: User) {
+        this.handleEventTrigger()
+    }
+
+    inputValue: string[] = []
+
+    handleEventTrigger(): void {
+        this.model.on('change', () => {
+            this.render()
+        })
+    }
+
+    
+    bindEvents(fragment: DocumentFragment): void {
+        const eventsData = this.bindData()
+        for (let events in eventsData) {
+            const [eventName, selector] = events.split(':')
+
+            const selectedElement = fragment.querySelectorAll(selector)
+
+            selectedElement.forEach((Element) => {
+                Element.addEventListener(eventName, (e) => {
+                    eventsData[events](e as KeyboardEvent)
+                })
+            })
+        }
+    }
+
+    render(): void {
+        this.parent!.innerHTML = ''
+        const htmlTemplate = document.createElement('template')
+
+        htmlTemplate.innerHTML = this.template()
+
+        this.bindEvents(htmlTemplate.content)
+        this.parent!.append(htmlTemplate.content)
+    }
+}
