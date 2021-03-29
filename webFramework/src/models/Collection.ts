bec72e498e39b15cd889ab32327cd93a4bae21a1
Igboanugwo Collins <munisco12@gmail.com>
2021-03-29 06:42:34 +0100

    working on user form

--- webFramework/src/models/Collection.ts
+++ webFramework/src/models/Collection.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from '../../node_modules/axios/index'
+import axios, { AxiosResponse } from 'axios'
 import { Events } from './Events'
 
 export class Collection<T, K> {
