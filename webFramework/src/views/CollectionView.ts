caf2c1e819b7cc0db66bc59cc526d3bf6e840aa1
Igboanugwo Collins <munisco12@gmail.com>
2021-03-30 11:30:52 +0100

    building collection view

--- webFramework/src/views/CollectionView.ts
+++ webFramework/src/views/CollectionView.ts
@@ -1,6 +1,6 @@
 import { Collection } from './../models/Collection';
-export abstract class CollectionView {
-    constructor(public parent: HTMLElement, public collection: Collection<>){
+export abstract class CollectionView<T, K> {
+    constructor(public parent: HTMLElement, public collection: Collection<T, K>){
 
     }
 }
