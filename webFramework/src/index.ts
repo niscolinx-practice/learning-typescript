64a2764f59b8f7428bec2948c821f41038c1cef4
Igboanugwo Collins <munisco12@gmail.com>
2021-03-30 11:40:03 +0100

    building collection view

--- webFramework/src/index.ts
+++ webFramework/src/index.ts
@@ -1,17 +1,17 @@
-import { UserEdit } from './views/UserEdit';
-import { User, UserProps } from './models/User';
-import { UserForm } from './views/UserForm'
+// import { UserEdit } from './views/UserEdit';
+// import { User, UserProps } from './models/User';
+// import { UserForm } from './views/UserForm'
 
-const root = document.getElementById('root')
-const user = User.buildUser({
-    name: 'Collins',
-    age: 31,
-})
-const userEdit = new UserEdit(root, user)
+// const root = document.getElementById('root')
+// const user = User.buildUser({
+//     name: 'Collins',
+//     age: 31,
+// })
+// const userEdit = new UserEdit(root, user)
 
-console.log({userEdit})
+// console.log({userEdit})
 
-userEdit.render()
+// userEdit.render()
 
 
 
