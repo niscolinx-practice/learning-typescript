3afd8c2564ca911008978d2345b929255cfdebc0
Igboanugwo Collins <munisco12@gmail.com>
2021-03-31 11:51:36 +0100

    Creating routes

--- Express/src/index.ts
+++ Express/src/index.ts
@@ -1,7 +1,12 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response } from 'express'
 
 const app = express()
 
-app.get('/', (req:Request, res:Response) => {
-    console.log({req})
+app.get('/', (req: Request, res: Response) => {
+    res.send(
+        `<div>
+        <h1>Hi there!</h1>  
+        </div>
+        `
+    )
 })
