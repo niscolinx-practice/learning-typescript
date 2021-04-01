98192ed2eed8321d19ba6fe579ebc721c48fdeb3
Igboanugwo Collins <munisco12@gmail.com>
2021-04-01 20:09:27 +0100

    working on decorator controllers

--- Express/src/AppRouter.ts
+++ Express/src/AppRouter.ts
@@ -4,6 +4,10 @@ export class AppRouter {
     private static instance: express.Router
 
     static getInstance(): express.Router{
-        
+        if(!AppRouter.instance){
+            AppRouter.instance = express.Router()
+        }
+
+        return AppRouter.instance
     }
 }
