ef5753c97d7ad33067da9e6e636f0ad517b53ff6
Igboanugwo Collins <munisco12@gmail.com>
2021-04-01 18:23:23 +0100

    working on decorator controllers

--- Express/src/index.ts
+++ Express/src/index.ts
@@ -1,18 +1,18 @@
-import { router as controllerRouter} from './controllers/decorators/controller';
+import { router as controllerRouter } from './controllers/decorators/controller'
 import bodyParser from 'body-parser'
 import express, { Request, Response } from 'express'
-import {router} from './routes/loginRoutes.js'
+import { router } from './routes/loginRoutes.js'
 import cookieSession from 'cookie-session'
-
-
+import '.controllers/LoginController'
 
 const app = express()
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(cookieSession({keys: ['kajdfadkfd']}))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieSession({ keys: ['kajdfadkfd'] }))
 app.use(router)
+app.use(controllerRouter)
 
 const PORT = 3000
 
-app.listen(PORT, () => { 
+app.listen(PORT, () => {
     console.log('listening on port', PORT)
 })
