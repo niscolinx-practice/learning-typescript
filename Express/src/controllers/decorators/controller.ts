23a9c82a47a3acc3e425f26ccd9c66d1c76ecf67
Igboanugwo Collins <munisco12@gmail.com>
2021-04-01 19:15:30 +0100

    working on decorator controllers

--- Express/src/controllers/decorators/controller.ts
+++ Express/src/controllers/decorators/controller.ts
@@ -4,11 +4,14 @@ export const controllerRouter = express.Router()
 
 export function controller(routePrefix: string) {
     return function (target: Function) {
-        console.log('hello there')
-        for (let key in target.prototype) {
+        console.log('hello there', target)
+        for (let key in target) {
+            console.log('prototype', key)
             const routeHandler = target.prototype[key]
 
             const path = Reflect.getMetadata('path', target.prototype, key)
+
+            console.log({path})
 
             if(path){
                 console.log(`${routePrefix}${path}`)
