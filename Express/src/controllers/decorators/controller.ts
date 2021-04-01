67aa5fb3cc23a5e8e8b2f9219de0c0172d23e13c
Igboanugwo Collins <munisco12@gmail.com>
2021-04-01 18:17:05 +0100

    Working with decorators and metadata

--- Express/src/controllers/decorators/controller.ts
+++ Express/src/controllers/decorators/controller.ts
@@ -1,11 +1,9 @@
-
- export function controller(routePrefix: string){
-     return function(target: Function){
-        for(let key in target.prototype){
+export function controller(routePrefix: string) {
+    return function (target: Function) {
+        for (let key in target.prototype) {
             const routeHandler = target.prototype[key]
 
             const path = Reflect.getMetadata('path', target.prototype, key)
-
         }
-     }
- }
+    }
+}
