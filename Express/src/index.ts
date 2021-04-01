883d36dd87846fb0b9f64c9b77a00aebe645db1a
Igboanugwo Collins <munisco12@gmail.com>
2021-04-01 18:54:21 +0100

    working on decorator controllers

--- Express/src/index.ts
+++ Express/src/index.ts
@@ -1,15 +1,16 @@
 //import { LoginController } from './controllers/LoginController';
-import { controllerRouter } from './controllers/decorators/controller'
+//const controllerRouter = require('./controllers/decorators/controller')
 import bodyParser from 'body-parser'
 import express, { Request, Response } from 'express'
 import { router } from './routes/loginRoutes.js'
 import cookieSession from 'cookie-session'
+import './controllers/LoginController.js'
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieSession({ keys: ['kajdfadkfd'] }))
-//app.use(router)
-app.use(controllerRouter)
+app.use(router)
+//app.use(controllerRouter)
 
 const PORT = 3000
 
