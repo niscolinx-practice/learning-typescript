24d0efb6e561a0f4d47fc23fee07769b1bd04526
Igboanugwo Collins <munisco12@gmail.com>
2021-04-01 20:49:43 +0100

    Working on use middleware

--- Express/src/controllers/decorators/use.ts
+++ Express/src/controllers/decorators/use.ts
@@ -4,6 +4,6 @@ import { MetadataKeys } from './MetadataKeys.js';
 export function use(middleware: RequestHandler){
 
     return function(target: any, key: string, desc: PropertyDescriptor){
-
+        const middlewares = Reflect.getMetadata(MetadataKeys.middleware, target, key) || []
     }
 }
