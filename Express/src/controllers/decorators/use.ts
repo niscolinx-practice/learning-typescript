800734c71ec8c74d24b10707b4f80fa06356c820
Igboanugwo Collins <munisco12@gmail.com>
2021-04-01 20:52:10 +0100

    Working on use middleware

--- Express/src/controllers/decorators/use.ts
+++ Express/src/controllers/decorators/use.ts
@@ -6,8 +6,6 @@ export function use(middleware: RequestHandler){
     return function(target: any, key: string, desc: PropertyDescriptor){
         const middlewares = Reflect.getMetadata(MetadataKeys.middleware, target, key) || []
 
-        middlewares.push(middleware)
-
-        Reflect.defineMetadata(MetadataKeys.middleware, middlewares, target, key)
+        Reflect.defineMetadata(MetadataKeys.middleware, [...middlewares, middleware], target, key)
     }
 }
