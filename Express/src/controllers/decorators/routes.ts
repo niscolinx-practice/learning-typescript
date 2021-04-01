0ec62f056ab368581de1c87cfa367a3ac6f9e667
Igboanugwo Collins <munisco12@gmail.com>
2021-04-01 14:08:33 +0100

    Working with decorators and metadata

--- Express/src/controllers/decorators/routes.ts
+++ Express/src/controllers/decorators/routes.ts
@@ -1,6 +1,8 @@
+import 'reflect-metadata'
+
 export function get(path: string) {
     return function(target: any, key: string, desc: PropertyDescriptor){
 
-    
+    Reflect.defineMetadata('path', )
     }
 }
