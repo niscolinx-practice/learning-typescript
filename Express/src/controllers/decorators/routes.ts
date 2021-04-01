5c9b5a9b53df5b0e953f9be1825b48e65a6f7032
Igboanugwo Collins <munisco12@gmail.com>
2021-04-01 20:20:11 +0100

    working on decorator controllers

--- Express/src/controllers/decorators/routes.ts
+++ Express/src/controllers/decorators/routes.ts
@@ -1,16 +1,12 @@
 import 'reflect-metadata'
 
-function routeBinder(method: string){
-
-    return function(path: string) {
-        return function(target: any, key: string, desc: PropertyDescriptor){
-    
-        Reflect.defineMetadata('path',path, target, key )
-        Reflect.defineMetadata('path',path, target, key )
+function routeBinder(method: string) {
+    return function (path: string) {
+        return function (target: any, key: string, desc: PropertyDescriptor) {
+            Reflect.defineMetadata('path', path, target, key)
+            Reflect.defineMetadata('method', method, target, key)
         }
-        
     }
-
 }
 
 export const get = routeBinder('get')
@@ -18,4 +14,3 @@ export const put = routeBinder('put')
 export const post = routeBinder('post')
 export const del = routeBinder('delete')
 export const patch = routeBinder('patch')
-
