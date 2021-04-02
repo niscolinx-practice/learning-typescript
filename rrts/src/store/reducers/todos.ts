d2cbd00b91abc2cd9a8bb06a042af2deb49588da
Igboanugwo Collins <munisco12@gmail.com>
2021-04-02 10:10:56 +0100

    Added action interface

--- rrts/src/store/reducers/todos.ts
+++ rrts/src/store/reducers/todos.ts
@@ -1,6 +1,12 @@
-import { Todo, FetchTodosAction } from './../actions/action';
-
+import { ActionTypes } from '../actions/types'
+import { Todo, FetchTodosAction } from './../actions/action'
 
 export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+    switch (action.type) {
+        case ActionTypes.fetchTodos:
+            return action.payload
 
+        default:
+            return state
+    }
 }
