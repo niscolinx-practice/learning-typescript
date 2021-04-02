693a1473ed2861d1b293f1dee775f49d1bd5a15c
Igboanugwo Collins <munisco12@gmail.com>
2021-04-02 10:24:25 +0100

    Added action interface

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import { Todo } from '../store/actions/action'
 
+interface AppProps {
+    todos: Todo[]
+    fetchTodos(): any
+}
 export class App extends React.Component {
     render(){
         return (
