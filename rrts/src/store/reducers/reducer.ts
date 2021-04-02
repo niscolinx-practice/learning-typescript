1be6dc6c2f5845790b140f30283abfbf1a6ce08e
Igboanugwo Collins <munisco12@gmail.com>
2021-04-02 10:15:12 +0100

    Added action interface

--- rrts/src/store/reducers/reducer.ts
+++ rrts/src/store/reducers/reducer.ts
@@ -1,7 +1,11 @@
+import { Todo } from './../actions/action';
 import { todosReducer } from './todos';
 import {combineReducers} from 'redux'
 
+export interface StoreState{
+    todos: Todo[]
+}
 
-export const reducers = combineReducers({
+export const reducers = combineReducers<StoreState>({
     todos: todosReducer
 })
