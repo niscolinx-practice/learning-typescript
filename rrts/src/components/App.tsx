48b1d3f933f71922e9ed6f07853b11e983ca92fc
Igboanugwo Collins <munisco12@gmail.com>
2021-04-02 10:32:09 +0100

    Added action interface

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import { Todo } from '../store/actions/action'
+import { Todo, fetchTodos } from '../store/actions/action'
 import { StoreState } from '../store/reducers/reducer'
 
 interface AppProps {
@@ -8,8 +8,13 @@ interface AppProps {
     fetchTodos(): any
 }
 class _App extends React.Component<AppProps> {
+
+    componentDidMount(){
+        this.props.fetchTodos()
+    }
+
     render(){
-        
+        console.log(this.props.todos)
         return (
             <div>This is the App</div>
         )
