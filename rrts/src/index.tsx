1d406401eacfb04f38678839223d32abdf29c369
Igboanugwo Collins <munisco12@gmail.com>
2021-04-02 08:11:34 +0100

    react initial setup

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-class App extends React.component {
+class App extends React.Component {
 
     render(){
+
+        return (
         <div>
             <h1>Hello dear</h1>
         </div>
+
+        )
     }
 }
 
-ReactDom.render(App, document.querySelector('#root'))
+ReactDom.render(<App/>, document.querySelector('#root'))
