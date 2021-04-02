be3a63cf38d0aa8e59af7f035a46025493b4339c
Igboanugwo Collins <munisco12@gmail.com>
2021-04-02 07:52:24 +0100

    Added react

--- Express/src/controllers/decorators/Methods.ts
+++
@@ -1,7 +0,0 @@
-export enum Methods {
-    get = 'get',
-    post = 'post',
-    del = 'delete',
-    patch = 'delete',
-    put = 'delete'
-}
--- Express/src/controllers/decorators/validateBody.ts
+++
@@ -1,6 +0,0 @@
-import { MetadataKeys } from './MetadataKeys.js';
-export function validateBody(...keys: string[]) {
-    return function(target: any, key: string, desc: PropertyDescriptor){
-        Reflect.defineMetadata(MetadataKeys.validator, keys, target, key)
-    }
-}
