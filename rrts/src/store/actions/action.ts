774d7a8a4b4cc3843b1b8cc8e7402bdc9e332db7
Igboanugwo Collins <munisco12@gmail.com>
2021-04-02 09:54:08 +0100

    Added actions

--- rrts/src/store/actions/action.ts
+++ rrts/src/store/actions/action.ts
@@ -1,8 +1,16 @@
 import axios from 'axios'
+import {Dispatch} from 'redux'
 
+
+const URL = 'https://jsonplaceholder.typicode.com/todos'
 export const fetchTodos = () => {
 
-    return (dispatch) => {
+    return async (dispatch: Dispatch) => {
+        const response = await axios.get(URL);
 
+        dispatch({
+            type: 'FETCH_TODOS',
+            payload: response.data
+        })
     }
 }
