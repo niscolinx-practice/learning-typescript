5a288b9817ce0106a7e73e078cdbbd3093f6c6f5
Igboanugwo Collins <munisco12@gmail.com>
2021-04-02 10:02:40 +0100

    Added actions

--- rrts/src/store/actions/action.ts
+++ rrts/src/store/actions/action.ts
@@ -8,13 +8,18 @@ interface Todo {
     completed: boolean
 }
 
+interface FetchTodosAction {
+    type: ActionTypes.fetchTodos
+    payload: Todo[]
+}
+
 const URL = 'https://jsonplaceholder.typicode.com/todos'
 export const fetchTodos = () => {
 
     return async (dispatch: Dispatch) => {
         const response = await axios.get<Todo[]>(URL);
 
-        dispatch({
+        dispatch<FetchTodosAction>({
             type: ActionTypes.fetchTodos,
             payload: response.data
         })
