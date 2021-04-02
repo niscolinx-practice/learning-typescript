b4298e7e3c440bba2a1a5bc421a5d15b82c18b9b
Igboanugwo Collins <munisco12@gmail.com>
2021-04-02 10:35:33 +0100

    connect to redux, added types and also shows all the todos

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -10,14 +10,21 @@ interface AppProps {
 class _App extends React.Component<AppProps> {
     
     onButtonClick = (): void => {
-        
         this.props.fetchTodos()
+    }
+
+    renderList(): JSX.Element[]{
+        return this.props.todos.map((todo: Todo) => {
+            return <div key={todo.id}>{todo.title}</div>
+        })
     }
 
     render(){
         console.log(this.props.todos)
         return (
-            <div><button onClick={this.onButtonClick}>Fetch</button></div>
+            <div><button onClick={this.onButtonClick}>Fetch</button>
+            {this.renderList}
+            </div>
         )
     }
 }
