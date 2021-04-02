9c053f153688971aaa5d862f0968082c0715af2d
Igboanugwo Collins <munisco12@gmail.com>
2021-04-02 16:04:45 +0100

    breaking out action creators

--- rrts/src/store/reducers/todos.ts
+++ rrts/src/store/reducers/todos.ts
@@ -1,6 +1,7 @@
+import { DeleteTodoAction } from './../actions/todos';
 import { Todo, FetchTodosAction, ActionTypes } from '../actions'
 
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: FetchTodosAction | DeleteTodoAction) => {
     switch (action.type) {
         case ActionTypes.fetchTodos:
             return action.payload
