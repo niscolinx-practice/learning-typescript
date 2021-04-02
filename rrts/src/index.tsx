f34bde9796290ec6705596c1a1aeffc07c1b0d1b
Igboanugwo Collins <munisco12@gmail.com>
2021-04-02 08:44:00 +0100

    Added redux

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,39 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
 
-interface AppProps {
-    color?: string
-}
+const store = createStore(reducers, applyMiddleware(thunk))
 
-class App extends React.Component<AppProps> {
+const app = (
+    <Provider store={store}>
+        <App/>
+    </Provider>
+)
 
-    state =  {counter: 0}
-    
-    constructor(props: AppProps){
-        super(props)
-
-        this.state = {counter: 0}
-    }
-
-    onIncrement = (): void => {
-        this.setState({counter: this.state.counter + 1})
-    }
-    onDecrement = (): void => {
-        this.setState({counter: this.state.counter - 1})
-    }
-    render(){
-
-        return (
-        <div>
-           <button onClick={this.onIncrement}>Increment</button>
-           <button onClick={this.onDecrement}>Decrement</button>
-
-           {this.state.counter}
-        </div>
-
-        )
-    }
-}
-
-ReactDom.render(<App/>, document.querySelector('#root'))
+ReactDom.render(app, document.querySelector('#root'))
