25ec646ef09e916fff6c5db32302748b96c0a87b
Igboanugwo Collins <munisco12@gmail.com>
2021-04-02 08:03:05 +0100

    react initial setup

---
+++ rrts/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+class App extends React.component {
+
+    render(){
+        <div>
+            <h1>Hello dear</h1>
+        </div>
+    }
+}
+
+ReactDom.render(App, document.querySelector('#root'))
